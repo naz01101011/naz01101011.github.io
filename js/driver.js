46af8eb113ed83a7ba08414442cfc1359cb4c02d
naz01101011 <naz.k@outlook.com>
2016-05-10 13:18:07 +0300

    edit

--- js/driver.js
+++ js/driver.js
@@ -9,9 +9,11 @@ function theDriver() {
 		var mymenu = document.getElementsByClassName("menu")[0];
 		mymenu.appendChild(button);
 
-		button.addEventListener ("click", function() {
-		  alert("did something");
-		});
+		button.addEventListener ("click", makeDiagram());
+	}
+	
+	function makeDiagram() {
+		document.getElementsByClassName("main-content")[0].innerHTML = "Aici vine diagrama"
 	}
 
 	//Calling the functions
