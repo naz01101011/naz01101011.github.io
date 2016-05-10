bf86206291a9d2eeffe09b39c6c7e353aa65a57e
naz01101011 <naz.k@outlook.com>
2016-05-10 13:14:28 +0300

    edit

--- js/driver.js
+++ js/driver.js
@@ -6,7 +6,7 @@ function theDriver() {
 		var button = document.createElement("button");
 		button.innerHTML = "Project Diagram";
 
-		var mymenu = document.getElementsByClassName("menu");
+		var mymenu = document.getElementsByClassName("menu")[0];
 		mymenu.appendChild(button);
 
 		button.addEventListener ("click", function() {
